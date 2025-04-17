e73d2a23f8c9a42d63aa818f8891a720bdbc12fd
gsingh704 <gurjant.dev@gmail.com>
2025-04-17 16:23:21 +0200

    refactor: update CSS styles and improve JSON structure for better readability

--- script.js
+++ script.js
@@ -22,81 +22,60 @@ fetch('en.json')
     .then(response => response.json())
     .then(data => {
         const resume = document.getElementById('resume');
-
         const { header, main } = data;
 
         // headerDiv
         const headerDiv = document.createElement('div');
         headerDiv.className = 'headerDiv';
         headerDiv.innerHTML = `
-            <img src="${header.picture.url}" alt="${header.name}" />
+            <img src="${header.picture.href}" alt="${header.picture.alt}" />
             <div class="header-details">
                 <h1 class="glitch" data-text="${header.name}">${header.name}</h1>
                 <p>${header.headline}</p>
-                <a <class="nowrap" href="mailto:${header.email}">${header.email}</a> | <a href="tel:${header.phone}" class="nowrap">${header.phone}</a> | <span class="nowrap">${header.location}</span>
+                <a class="nowrap" href="mailto:${header.email}">${header.email}</a> | 
+                <a href="tel:${header.phone}" class="nowrap">${header.phone}</a> | 
+                <span class="nowrap">${header.location}</span>
                 <p class="links">
                     ${header.links.map(link => `<a href="${link.href}" target="_blank">${link.label}</a>`).join('')}
                 </p>
+                <p class="summary">${header.summary}</p>
             </div>
         `;
         resume.appendChild(headerDiv);
 
-        // Summary
-
-        // Loop through all main where item length is greater than 0
-        for (let section in main) {
-            const sectionData = main[section];
-
+        // Render main sections
+        Object.keys(main).forEach(sectionKey => {
+            const sectionData = main[sectionKey];
             const sectionElement = document.createElement('div');
-            sectionElement.className = 'section';
-            sectionElement.classList.add('section', section);
-            sectionElement.innerHTML = `
-                <h2>${section}</h2>
-            `;
+            sectionElement.className = `section ${sectionKey}`;
+            sectionElement.innerHTML = `<h2>${sectionKey}</h2>`;
 
-            // Create the item_list container
             const itemListElement = document.createElement('div');
             itemListElement.className = 'item_list';
 
-            for (let item in sectionData.items) {
-                const itemData = sectionData.items[item];
+            sectionData.forEach(item => {
                 const itemElement = document.createElement('div');
                 itemElement.className = 'item';
-                const url = itemData.url;
 
                 itemElement.innerHTML = `
-                <div class="subtitle">
-                    ${url ? `<a href="${url}" target="_blank">` : ''}
-                        ${itemData.title ? `<h3>${itemData.title}</h3>` : ''}
-                    ${url ? `</a>` : ''}
-                    ${url ? `<a href="${url}" target="_blank">` : ''}
-                        ${itemData.subtitle ? `<h3 class="sub">${itemData.subtitle}</h3>` : ''}
-                    ${url ? `</a>` : ''}
-                    ${itemData.tags ?
-                        `<div class="item-tags"> ${itemData.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}</div> `
-                        : ''}
-                </div>
-                <div class="subtitle">
-                    ${itemData.date ? `<p>${itemData.date}</p>` : ''}
-                    ${itemData.location ? `<p>${itemData.location}</p>` : ''}
-                </div>
-                ${itemData.score ? `<p>${itemData.score}</p>` : ''}
-                ${itemData.img ? `<div class="item-image">${itemData.img.map(img => `<img src="${img.src}" alt="${img.alt}" class="${img.class}" />`).join('')}</div>` : ''}
-                ${itemData.description ? `<p>${itemData.description}</p>` : ''}
-                ${itemData.links ? `<p class="links">
-                ${itemData.links.map(link => `<a href="${link.href}" target="_blank">${link.label}</a>`).join('')}
-               </p>` : ''}
-                `;
+                    <div class="subtitle">
+                        ${item.href ? `<a href="${item.href}" target="_blank"><h3>${item.title}</h3></a>` : `<h3>${item.title}</h3>`}
+                        ${item.subtitle ? `<h4 class="sub">${item.subtitle}</h4>` : ''}
+                    </div>
+                    ${item.date ? `<p>${item.date}</p>` : ''}
+                    ${item.location ? `<p>${item.location}</p>` : ''}
+                    ${item.description ? `<p>${item.description}</p>` : ''}
+                    ${item.tags ? `<div class="item-tags">${item.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}</div>` : ''}
+                    ${item.img ? `<div class="item-image">${item.img.map(img => `<img src="${img.src}" alt="${img.alt}" class="${img.class}" />`).join('')}</div>` : ''}
+                    ${item.links ? `<p class="links">${item.links.map(link => `<a href="${link.href}" target="_blank">${link.label}</a>`).join('')}</p>` : ''}
+                    `;
 
-                // Append each itemElement to the item_list
                 itemListElement.appendChild(itemElement);
-            }
+            });
 
-            // Append the item_list to the sectionElement
             sectionElement.appendChild(itemListElement);
-
-            // Append the sectionElement to the resume
             resume.appendChild(sectionElement);
-        }
+        });
     })
+
     .catch(error => console.error('Error fetching resume data:', error));
